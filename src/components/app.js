c76c9b5ca0cda032eb8a776a53d70d4eb8e64577
ergokira <josuegb15@gmail.com>
2017-02-07 14:47:08 -0800

    base title

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import BookList from '../containers/book-list';
 import BookDetail from '../containers/book-detail';
+import Header from './header';
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
+        <Header />
         <BookList />
         <BookDetail />
       </div>
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <h1>Books Gallery Web App</h1>
+    );
+};
+
+export default Header;
