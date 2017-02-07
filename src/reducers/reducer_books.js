f5ca28bfc6cbc2d302403a280c6dd4e5f0000510
ergokira <josuegb15@gmail.com>
2017-02-07 14:06:39 -0800

    Base modification

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,28 @@
 export default function(){
     return [
-        { title: 'JavaScript: The Good Parts', author:'Douglas Crockford', pages: 170, img:'img/book1.jpg'},
-        { title: 'Learning JavaScript Design Patterns', author:'Addy Osmani', pages: 254, img:'img/book2.jpg'},
-        { title: 'HTML5 Pocket Reference', author:'Jennifer Niederst Robbins', pages: 184, img:'img/book3.jpg'},
-        { title: 'Secrets of the JavaScript Ninja', author:'John Resig', pages: 300, img:'img/book4.jpg'}
+        { 
+            title: 'JavaScript: The Good Parts', 
+            author:'Douglas Crockford', 
+            pages: 170, 
+            img:'img/book1.jpg'
+        },
+        { 
+            title: 'Learning JavaScript Design Patterns', 
+            author:'Addy Osmani', 
+            pages: 254, 
+            img:'img/book2.jpg'
+        },
+        { 
+            title: 'HTML5 Pocket Reference', 
+            author:'Jennifer Niederst Robbins', 
+            pages: 184, 
+            img:'img/book3.jpg'
+        },
+        { 
+            title: 'Secrets of the JavaScript Ninja', 
+            author:'John Resig', 
+            pages: 300, 
+            img:'img/book4.jpg'
+        }
     ];
 }
